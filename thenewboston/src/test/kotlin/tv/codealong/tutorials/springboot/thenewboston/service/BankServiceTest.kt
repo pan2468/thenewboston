ca9d245b851d86ec3a99b4bfe804b34c32e7da6a
임채성 <pan1129@naver.com>
2022-06-02 15:45:24 +0900

    Controller Commit

--- thenewboston/src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/service/BankServiceTest.kt
+++ thenewboston/src/test/kotlin/tv/codealong/tutorials/springboot/thenewboston/service/BankServiceTest.kt
@@ -1,15 +1,13 @@
 package tv.codealong.tutorials.springboot.thenewboston.service
 
-import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import tv.codealong.tutorials.springboot.thenewboston.datasource.BankDataSource
 
 internal class BankServiceTest{
 
-    private  val dataSource: BankDataSource = mockk(relaxed = true)
+    private val dataSource: BankDataSource = mockk(relaxed = true) // mockk kotlin 테스트 동작 라이브러리
     private val bankService = BankService(dataSource)
 
     @Test
@@ -18,7 +16,9 @@ internal class BankServiceTest{
         bankService.getBanks()
 
         // then
-        verify(exactly = 1) { dataSource.retrieveBanks() }
+        verify(exactly = 1) { //test 안에 검증확인
+            dataSource.retrieveBanks()
+        }
 
     }
 
